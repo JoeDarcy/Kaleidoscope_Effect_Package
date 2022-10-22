af77e284e258ccbe6ec2af3baaa519fd5a438beb
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2022-10-22 12:52:04 +0100

    Reverted changes.

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+Cahnge Log
+
+This is the change log for this package.
---
+++ Runtime/CameraFlip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFlip : MonoBehaviour
+{
+	[SerializeField] private bool flip = false;
+	[Range(-1,1)]
+	[SerializeField] private float xFlip = 1;
+	[Range(-1, 1)]
+	[SerializeField] private float yFlip = 1;
+
+	private Camera cameraFlip;
+	private Matrix4x4 originalProjectionMatrix;
+
+	private void Start()
+	{
+		// Get camera on game object
+		cameraFlip = GetComponent<Camera>();
+
+		originalProjectionMatrix = cameraFlip.projectionMatrix;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+	    if (flip)
+	    {
+		    cameraFlip.projectionMatrix = originalProjectionMatrix * Matrix4x4.Scale(new Vector3(xFlip, yFlip, 1)); 
+			flip = false;
+		}
+    }
+}
